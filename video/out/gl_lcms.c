cd7d76efa215639da09fca9903009147327c3187
Niklas Haas <git@nand.wakku.to>
2015-04-29 18:43:13 +0200

    vo_opengl: gl_lcms: make ICC loading less verbose
    Especially with the new ICC cache rework, you get a lot of ugly output
    messages that don't really contain any meaningful content.

--- video/out/gl_lcms.c
+++ video/out/gl_lcms.c
@@ -122,7 +122,7 @@ static bool load_profile(struct gl_lcms *p)
     if (!p->icc_path)
         return false;
 
-    MP_INFO(p, "Opening ICC profile '%s'\n", p->icc_path);
+    MP_VERBOSE(p, "Opening ICC profile '%s'\n", p->icc_path);
     struct bstr iccdata = load_file(p, p->icc_path, p->global);
     if (!iccdata.len)
         return false;
@@ -241,7 +241,7 @@ bool gl_lcms_get_lut3d(struct gl_lcms *p, struct lut3d **result_lut3d)
 
     // check cache
     if (cache_file) {
-        MP_INFO(p, "Opening 3D LUT cache in file '%s'.\n", cache_file);
+        MP_VERBOSE(p, "Opening 3D LUT cache in file '%s'.\n", cache_file);
         struct bstr cachedata = load_file(tmp, cache_file, p->global);
         if (bstr_eatstart(&cachedata, bstr0(LUT3D_CACHE_HEADER))
             && bstr_eatstart(&cachedata, bstr0(cache_info))
