daoopp/mpv

video-to-display syncing (WIP)

player/core.h
@@ -236,6 +236,13 @@ typedef struct MPContext {
     double playback_speed;
     // Additional speed factor chosen by display sync code.
     double speed_correction;
+    double audio_speed_correction;
+    double last_frame_duration;
+    bool display_sync_active;
+    // Timing error (in seconds) due to rounding on vsync boundaries
+    double display_sync_error;
+    // Last time speed was adjusted (in mp_time_us() units)
+    int64_t last_display_sync_adjustment;
     /* Set if audio should be timed to start with video frame after seeking,
      * not set when e.g. playing cover art */
     bool sync_audio_to_video;

haasn/mpvhq-old

Conflicts:
	player/video.c
	video/out/vo.c
	video/out/vo.h
	video/out/vo_opengl.c
	video/out/vo_opengl_cb.c
	video/out/vo_vdpau.c

player/core.h
@@ -238,6 +238,13 @@ typedef struct MPContext {
     double playback_speed;
     // Additional speed factor chosen by display sync code.
     double speed_correction;
+    double audio_speed_correction;
+    bool display_sync_active;
+    double timestamp_unrounding_error;
+    // Timing error (in seconds) due to rounding on vsync boundaries
+    double display_sync_error;
+    // Last time speed was adjusted (in mp_time_us() units)
+    int64_t last_display_sync_adjustment;
     /* Set if audio should be timed to start with video frame after seeking,
      * not set when e.g. playing cover art */
     bool sync_audio_to_video;